37cbbe6d8cde33304a302b1c46e1f46c0c69c8fe
Chakkaradeep (Chaks) CC <chakkaradeep@users.noreply.github.com>
2017-09-20 06:25:19 -0700

    JqueryDataTables Web Part

---
+++ src/webparts/jqueryDataTables/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJqueryDataTablesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'JqueryDataTablesWebPartStrings' {
+  const strings: IJqueryDataTablesWebPartStrings;
+  export = strings;
+}
